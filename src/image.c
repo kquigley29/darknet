208072997d83013b86addf2d8e9fdb3bbda751b1
keane <keaneq@protonmail.com>
2020-11-13 20:13:48 +0000

    generalise load_alphabet darknet abs path

--- src/image.c
+++ src/image.c
@@ -273,9 +273,7 @@ image **load_alphabet()
         alphabets[j] = (image*)xcalloc(128, sizeof(image));
         for(i = 32; i < 127; ++i){
             char buff[256];
-//            sprintf(buff, "data/labels/%d_%d.png", i, j);
-
-            sprintf(buff, "/home/keane/Projects/darknet/data/labels/%d_%d.png", i, j);
+            sprintf(buff, "%s/data/labels/%d_%d.png", DARKNET, i, j);
             alphabets[j][i] = load_image_color(buff, 0, 0);
         }
     }
